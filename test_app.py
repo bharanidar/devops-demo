8873563bda6a32a55e7c03b01cd86874fdcd1810
Bharanidar <bharanidar@192.168.1.165>
2026-09-18 15:44:40 +0530

    SCRUM-6: Implement application health check

---
+++ test_app.py
@@ -0,0 +1,19 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.get_json()["message"] == "DevOps Demo Application"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.get_json()["status"] == "healthy"
